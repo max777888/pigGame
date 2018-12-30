17e5f4c4863b6c34e16f9db47678211861d1c5e1
inader <issamnader@gmail.com>
2018-12-31 01:23:16 +0200

    display output using json stringify

--- node/weather-app/app.js
+++ node/weather-app/app.js
@@ -3,5 +3,5 @@ request({
 	url:'https://maps.googleapis.com/maps/api/geocode/json?address=48+eastwood+ave,+scarborough,+CA&key=AIzaSyAuybMg3Qx-5ExSmSl0qRoQGqvlwKN4Rg8',
 	json:true
 },(error, response, body)=>{
-  console.log(body);
+  console.log(JSON.stringify(body , undefined , 2));
 });
