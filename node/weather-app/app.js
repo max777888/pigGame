aa790fc5e75bc54dfce48829837ec40a9a58825d
inader <issamnader@gmail.com>
2018-12-31 18:21:00 +0200

    add yargs

--- node/weather-app/app.js
+++ node/weather-app/app.js
@@ -3,5 +3,8 @@ request({
 	url:'https://maps.googleapis.com/maps/api/geocode/json?address=48+eastwood+ave,+scarborough,+CA&key=AIzaSyAuybMg3Qx-5ExSmSl0qRoQGqvlwKN4Rg8',
 	json:true
 },(error, response, body)=>{
-  console.log(JSON.stringify(body , undefined , 2));
+  // console.log(JSON.stringify(response , undefined , 2));
+   console.log(body.results[0].geometry.location.lat);
+   console.log(body.results[0].geometry.location.lng);
+   
 });
