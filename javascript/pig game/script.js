93fc2478bfa00b6eff500b9b5d0981f151f6c476
inader <issamnader@gmail.com>
2019-01-24 01:09:00 +0200

    initial

---
+++ javascript/pig game/script.js
@@ -0,0 +1,14 @@
+console.log('fuck off from script.js');
+console.log('test it');
+
+console.log('test');
+var age=18;
+age>=18 ? console.log('greater than'):console.log('fuck off again');
+var jonteamavr = 89+120+103/3;
+var miketeamavr= 116+94+123/3;
+jonteamavr > miketeamavr ? console.log('jon win'):console.log('mike win');
+var john={
+   firstName : 'john',
+   lastName:'bocch'
+};
+console.log(john.firstName);
